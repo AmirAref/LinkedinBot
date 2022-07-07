9e996d440b48568568661976f0b5a1a90d32cf1c
Amir Aref <amiraref808@gmail.com>
2022-07-08 01:54:14 +0430

    validation the url pattern by regex

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from pyrogram import Client, filters, types
 import requests
 import json
+import re
 
 # configuration
 
@@ -28,12 +29,38 @@ def get_video_links(url : str):
     # out
     return links
 
+# check validition url
+def validition_url(url):
+    check = re.compile(
+            r'^(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    #out
+    if check.match(url):
+        return True
+    else:
+        return False
+
+
 # message handler
 # get only text messages from private chats
 @app.on_message(filters.text & filters.private)
 async def message_handler(client : Client, message : types.Message):
+    # check the url pattern
+    text = message.text
+    if not validition_url(text):
+        return await message.reply("Invalid Url !")
+    
     # get the video links of video
-    links = get_video_links(message.text)
+    try:
+        links = get_video_links(text)
+    except Exception as e:
+        print(e)
+        return message.reply("an error occurred !")
+    # output
     await message.reply("\n".join(links))
 
 
